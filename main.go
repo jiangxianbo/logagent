cd2f791b9a96f2ab8ad1c13dd4c1239a0742135a
jiangxianbo <jxb42359@163.com>
2022-07-28 23:47:52 +0800

    kafka库 tail库 ini包

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"gopkg.in/ini.v1"
+	"logagent/conf"
 	"logagent/kafka"
 	"logagent/taillog"
 	"time"
+)
+
+var (
+	cfg = new(conf.AppConf)
 )
 
 // logAgent 入口程序
@@ -15,7 +21,7 @@ func run() {
 		select {
 		case line := <-taillog.ReadChan():
 			// 2.发送到kafka
-			kafka.SendToKafka("web_log", line.Text)
+			kafka.SendToKafka(cfg.KafkaConf.Topic, line.Text)
 		default:
 			time.Sleep(time.Second)
 		}
@@ -24,15 +30,22 @@ func run() {
 }
 
 func main() {
+	// 0.加载配置文件
+	err := ini.MapTo(cfg, "./conf/config.ini")
+	if err != nil {
+		fmt.Printf("load ini failed, err:%v\n", err)
+		return
+	}
+
 	// 1.初始化kafka连接
-	err := kafka.Init([]string{"127.0.0.1:9092"})
+	err = kafka.Init([]string{cfg.KafkaConf.Address})
 	if err != nil {
 		fmt.Printf("init kafka failed, err:%v\n", err)
 		return
 	}
 	fmt.Println("init kafka sucess!")
 	// 2.打开日志文件准备收集文件
-	err = taillog.Init("./my.log")
+	err = taillog.Init(cfg.TaillogConf.FileName)
 	if err != nil {
 		fmt.Printf("init taillog failed, err:%v\n", err)
 		return
